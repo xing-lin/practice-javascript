fc8dab36aeabdf97291e7ca9f2eb85a27440286a
linxing <linxing0427@163.com>
2021-11-12 13:45:30 +0800

    update es-demo

--- src/design-pattern/observer.js
+++ src/design-pattern/observer.js
@@ -42,3 +42,29 @@ salesOffices.listen("squareMeter88", function (price) {
 
 salesOffices.trigger("squareMeter88", 200000);
 salesOffices.trigger("squareMeter100", 200000);
+
+// 使用Proxy实现观察者模式
+const queuedObservers = new Set();
+
+const observe = (fn) => queuedObservers.add(fn);
+
+const observable = (obj) =>
+  new Proxy(obj, {
+    set(target, key, value, receiver) {
+      const result = Reflect.set(target, key, value, receiver);
+      queuedObservers.forEach((observer) => observer());
+      return result;
+    },
+  });
+
+const person = observable({
+  name: "张三",
+  age: 20,
+});
+
+function print() {
+  console.log(`person->${person.name}, ${person.age}`);
+}
+observe(print);
+
+person.name = "李四";
